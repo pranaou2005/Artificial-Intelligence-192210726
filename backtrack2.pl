85ad1a2892d7be3b69ae001f4958a624f8a7f722
pranaou2005 <148045337+pranaou2005@users.noreply.github.com>
2024-03-22 07:38:30 +0530

    Add files via upload

---
+++ backtrack2.pl
@@ -0,0 +1,12 @@
+% Define fruits and their colors
+fruit_color(apple, red).
+fruit_color(banana, yellow).
+fruit_color(grape, purple).
+fruit_color(orange, orange).
+fruit_color(watermelon, green).
+fruit_color(lemon, yellow).
+fruit_color(blueberry, blue).
+
+% Query to find the color of a specific fruit
+% Example Query: ?- fruit_color(apple, Color).
+% This will find the color of the fruit "apple"
